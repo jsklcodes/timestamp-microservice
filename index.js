c73e302a24fcd58deb02daebd95526987d3fd980
Kallyne Layane <hellookallyne@gmail.com>
2023-10-03 10:44:45 -0300

    Fix CSS import bug

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -11,11 +12,11 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // your first API endpoint...
