20962bdf29bff7a5c885fa82299f35a75107e2fa
Kallyne Layane <hellookallyne@gmail.com>
2023-10-03 10:38:30 -0300

    Add timestamp endpoint

--- index.js
+++ index.js
@@ -6,25 +6,66 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+// timestamp endpoint
+app.get('/api/:date?', (req, res) => {
+  let dateParam = req.params.date;
 
+  const validateDate = passedValue => {
+    const date = new Date(passedValue);
+    let isValid = true;
+
+    if (isNaN(date) || !isFinite(date)) {
+      isValid = false;
+    }
+
+    return { passedDate: date, isValid };
+  };
+
+  let timestamp;
+
+  if (!dateParam) {
+    timestamp = {
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString(),
+    };
+
+    return res.json(timestamp);
+  }
+
+  if (/^\d+$/.test(dateParam)) {
+    dateParam = +dateParam;
+  }
+
+  const { passedDate, isValid } = validateDate(dateParam);
+
+  if (isValid) {
+    timestamp = {
+      unix: passedDate.getTime(),
+      utc: passedDate.toUTCString(),
+    };
+
+    res.json(timestamp);
+  } else {
+    res.json({ error: 'Invalid Date' });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
